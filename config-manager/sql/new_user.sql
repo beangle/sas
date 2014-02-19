ca0b639b919a5baca41d0ff7f263a4038efda7b9
duantihua <duantihua@gmail.com>
2014-02-19 23:16:01 +0800

    a

--- config-manager/sql/new_user.sql
+++
@@ -1,4 +0,0 @@
-DROP USER eams_empty cascade;
-create user eams_empty identified by 1 default tablespace users temporary tablespace temp;
-grant connect,resource,unlimited tablespace,create any sequence,drop any sequence to eams_empty;
-exit
