d9edcbc1bbd51087924ff3dc96e45da1414662f4
chaostone <duantihua@gmail.com>
2014-10-22 19:29:44 +0800

    refactor:rename local variable

--- jdbc/src/main/scala/org/beangle/tomcat/jdbc/EncryptedDataSourceFactory.scala
+++ jdbc/src/main/scala/org/beangle/tomcat/jdbc/EncryptedDataSourceFactory.scala
@@ -24,19 +24,20 @@ class EncryptedDataSourceFactory extends DataSourceFactory {
   override def createDataSource(properties: ju.Properties, context: Context, XA: Boolean): DataSource = {
     val url = properties.get("url").asInstanceOf[String]
     if (null != url && url.startsWith("http")) properties.putAll(parse(getResponseText(new URL(url))))
+
     val poolProperties = DataSourceFactory.parsePoolProperties(properties)
-    val encodedPassword = poolProperties.getPassword
-    val name = poolProperties.getName
-    val secretKey = System.getenv(name.replace("/", "_") + "_secret")
+    val encodedPwd = poolProperties.getPassword
+    val keyName = poolProperties.getName.replace("/", "_") + "_secret"
+    val secretKey = System.getenv(keyName)
     val password =
-      if (encodedPassword.startsWith("?")) {
-        if (null == secretKey) decryptByPrompt(encodedPassword.substring(1))
-        else new Encryptor(secretKey).decrypt(encodedPassword.substring(1))
+      if (encodedPwd.startsWith("?")) {
+        if (null == secretKey) decryptByPrompt(keyName, encodedPwd.substring(1))
+        else new Encryptor(secretKey).decrypt(encodedPwd.substring(1))
       } else {
-        new Encryptor(secretKey).decrypt(encodedPassword)
+        new Encryptor(secretKey).decrypt(encodedPwd)
       }
     poolProperties.setPassword(password)
-    // The rest of the code is copied from Tomcat's DataSourceFactory.
+
     if (poolProperties.getDataSourceJNDI != null && poolProperties.getDataSource == null) performJNDILookup(context, poolProperties)
     val dataSource = if (XA) new XADataSource(poolProperties) else new org.apache.tomcat.jdbc.pool.DataSource(poolProperties)
     dataSource.createPool()
@@ -46,20 +47,20 @@ class EncryptedDataSourceFactory extends DataSourceFactory {
   /**
    * decrypt password using user input.
    */
-  private def decryptByPrompt(password: String): String = {
+  private def decryptByPrompt(keyName: String, encodedPwd: String): String = {
     var i = 0
     var correctSecret = false
     var decoded: String = null
     val br = new BufferedReader(new InputStreamReader(System.in))
-    Console.print("What is the data_source_secret key:")
+    Console.print(s"What is the $keyName:")
     while (i < 3 && !correctSecret) {
       var secretKey: String = null
       try {
         secretKey = br.readLine()
         if (null == secretKey) {
-          Console.print("What is the data_source_secret key:")
+          Console.print(s"What is the $keyName key:")
         } else {
-          decoded = new Encryptor(secretKey).decrypt(password)
+          decoded = new Encryptor(secretKey).decrypt(encodedPwd)
           correctSecret = true
         }
       } catch {
@@ -67,7 +68,7 @@ class EncryptedDataSourceFactory extends DataSourceFactory {
       }
       i += 1
     }
-    if (null == decoded) throw new RuntimeException("Cannot decoded " + password)
+    if (null == decoded) throw new RuntimeException(s"Cannot decoded $encodedPwd")
     decoded
   }
 
