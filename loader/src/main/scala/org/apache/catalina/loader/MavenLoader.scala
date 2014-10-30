b2e777e00643b0d7a9f278a8ec87ead3d8a33478
mychii <mychii@qq.com>
2014-10-30 17:12:06 +0800

    complete mavenLoader

--- loader/src/main/scala/org/apache/catalina/loader/MavenLoader.scala
+++ loader/src/main/scala/org/apache/catalina/loader/MavenLoader.scala
@@ -2,80 +2,151 @@ package org.apache.catalina.loader
 
 import java.io.LineNumberReader
 import java.io.FileReader
-import org.apache.catalina.loader.WebappLoader
-import org.apache.catalina.loader.WebappClassLoader
 import java.io.File
 import javax.servlet.ServletContext
 import org.apache.catalina.Context
 import java.net.URL
+import java.io.IOException
+import scala.collection.mutable.ListBuffer
+import java.util.StringTokenizer
+import java.io.FileOutputStream
+import java.io.InputStream
+import scala.xml.Null
+import java.io.OutputStream
 
-class MavenLoader(parent:ClassLoader )   extends WebappLoader(parent){
+class MavenLoader(parent: ClassLoader) extends WebappLoader(parent) {
 
-       var webClassPathFile = ".#webclasspath";
-       var tomcatPluginFile = ".tomcatplugin";
+  var webClassPathFile = "WEB-INF/webclasspath"
+  var tomcatPluginFile = ".tomcatplugin"
+  var repositoryPath = System.getProperties().getProperty("user.home") + "/.m2/repository/"
+  if (!new File(repositoryPath).exists()) {
+    new File(repositoryPath).mkdirs()
+  }
 
-    def this()    {
-        this(null)
+  def this() {
+    this(null)
+  }
+
+  override def startInternal() {
+    log("Starting DevLoader")
+    super.startInternal()
+    val cl = super.getClassLoader()
+    if (!(cl.isInstanceOf[WebappClassLoader])) {
+      logError("Unable to install WebappClassLoader !")
+    } else {
+      val devCl = cl.asInstanceOf[WebappClassLoader]
+      val classpath = new StringBuffer()
+      readWebClassPathEntries() foreach { url =>
+        log(url)
+        devCl.addURL(new File(url).toURL())
+      }
     }
+  }
 
-    def  startInternal(){
-        log("Starting DevLoader");
-        super.startInternal();
-        val cl = super.getClassLoader();
-        if(!(cl.isInstanceOf[WebappClassLoader]))        {
-            logError("Unable to install WebappClassLoader !");
-            return;
+  protected def log(msg: String) {
+    println((new StringBuilder("[MavenLoader] ")).append(msg).toString())
+  }
+
+  protected def logError(msg: String) {
+    System.err.println((new StringBuilder("[MavenLoader] Error: ")).append(msg).toString())
+  }
+
+  protected def readWebClassPathEntries(): List[String] = {
+    val prjDir = new File(servletContext.getRealPath("/"))
+    val urls = loadWebClassPathFile(prjDir)
+    downloadJars(urls)
+    urls
+  }
+
+  protected def loadWebClassPathFile(prjDir: File): List[String] = {
+    val cpFile = new File(prjDir, this.webClassPathFile)
+    if (cpFile.exists()) {
+      var reader: FileReader = null
+      try {
+        val rc = new ListBuffer[String]()
+        reader = new FileReader(cpFile)
+        val lr = new LineNumberReader(reader)
+        var line: String = null
+        do {
+          line = lr.readLine()
+          if (line != null) {
+            line = line.replace('\\', '/')
+            rc += repositoryPath + line
+          }
+        } while (line != null)
+        rc.toList
+      } catch {
+        case ioEx: IOException =>
+          ioEx.printStackTrace()
+          null
+      }
+    } else null
+  }
+
+  private def downloadJars(urls: List[String]) {
+    val downloadList = new ListBuffer[Tuple2[String, String]]()
+    urls.foreach(url => {
+      if (!new File(url).exists()) {
+        val pair = ("http://central.maven.org/maven2/" + url.toString().substring(repositoryPath.length()), url.toString())
+        downloadList += pair
+      }
+    })
+
+    val threads = new ListBuffer[Downloader]()
+    while (downloadList.size > 0 || threads.size > 0) {
+      print("\b" * 1000)
+      if (threads.size < 4 && downloadList.size > 0) {
+        val pair = downloadList.remove(0)
+        println("download:" + pair._1)
+        val thread = new Downloader(threads, pair)
+        thread.start()
+        threads += thread
+      } else {
+        Thread.sleep(1000)
+      }
+      threads.foreach(t => print(t.count / 1024 + "KB/" + (t.fileSize / 1024) + "KB    "))
+      print(" " * 100)
+    }
+  }
+
+  class Downloader(threads: ListBuffer[Downloader], pair: Tuple2[String, String]) extends Thread {
+    var fileSize, count = 0
+    private val eof = -1
+    override def run() {
+      var input: InputStream = null
+      var output: OutputStream = null
+      try {
+        val url = new URL(pair._1)
+        val file = new File(pair._2 + ".tmp")
+        val buffer = new Array[Byte](1024 * 4)
+        val conn = url.openConnection()
+        fileSize = conn.getContentLength()
+        val input = url.openConnection().getInputStream()
+        if (!file.getParentFile().exists()) {
+          file.getParentFile().mkdirs()
         }
-        val devCl = cl.asInstanceOf[WebappClassLoader]
-        readWebClassPathEntries() foreach {url => 
-         devCl.addURL(url)
+        val output = new FileOutputStream(file)
+        var n = input.read(buffer)
+        while (eof != n) {
+          output.write(buffer, 0, n)
+          count += n
+          n = input.read(buffer)
         }
+        file.renameTo(new File(pair._2))
+      } catch {
+        case t =>
+          t.printStackTrace()
+          if (input != null) input.close()
+          if (output != null) output.close()
+      }
+      threads -= this
     }
+  }
 
-    protected def log(msg:String )
-    {
-        println((new StringBuilder("[DevLoader] ")).append(msg).toString());
-    }
-
-    protected def logError(msg:String )
-    {
-        System.err.println((new StringBuilder("[DevLoader] Error: ")).append(msg).toString());
-    }
-
-    protected def readWebClassPathEntries():List[URL]=  {
-        val prjDir =   new File(servletContext.getRealPath("/"))
-        if(prjDir == null)
-            return List.empty
-          loadWebClassPathFile(prjDir)
-    }
-
-    protected def loadWebClassPathFile(prjDir:File ):List[String]=     {
-        File cpFile;
-        FileReader reader;
-        cpFile = new File(prjDir, webClassPathFile);
-        if(!cpFile.exists())
-            break MISSING_BLOCK_LABEL_99;
-        reader = null;
-        List rc;
-        rc = new ArrayList();
-        reader = new FileReader(cpFile);
-        LineNumberReader lr = new LineNumberReader(reader);
-        for(String line = null; (line = lr.readLine()) != null;)
-        {
-            line = line.replace('\\', '/');
-            rc.add(line);
-        }
-
-        return rc;
-        IOException ioEx;
-        ioEx;
-        if(reader == null);
-        return null;
-        return null;
-    }
-
-    protected def servletContext:ServletContext = 
-    {
-      this.getContext.getServletContext
-    }
+  protected def servletContext: ServletContext = {
+    //tomcat 8.0
+    this.getContext.getServletContext
+    //tomcat 7.0
+    //    getContainer().asInstanceOf[Context].getServletContext()
+  }
 }
