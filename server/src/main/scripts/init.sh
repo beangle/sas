75a8463a90e1209736647bdbe009e4e3bc32810c
chaostone <duantihua@gmail.com>
2015-01-27 16:12:25 +0800

    support config start up

--- server/src/main/scripts/init.sh
+++ server/src/main/scripts/init.sh
@@ -2,4 +2,9 @@
 
 bin/get-scala.sh 2.11.2
 bin/get-pg-driver.sh
+bin/get-lib.sh org.beangle.tomcat beangle-tomcat-core 0.1.1
+bin/get-ext.sh org.beangle.commons beangle-commons-core 4.2.2
+bin/get-ext.sh org.beangle.tomcat beangle-tomcat-configer 0.1.1
+bin/get-ext.sh org.freemarker freemarker 2.3.20
+bin/get-ext.sh org.slf4j slf4j-api 1.7.7
 bin/install.sh
