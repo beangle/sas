137156cb1b56f911d2f1b25758291f8d3ec54a58
chaostone <duantihua@gmail.com>
2016-06-08 08:14:28 +0800

    change tomcat version

--- server/src/main/scripts/init.sh
+++ server/src/main/scripts/init.sh
@@ -35,4 +35,4 @@ install_lib_of_versions org.scala-lang.modules scala-xml_2.12.0-M4 scalaxml_vers
 ./install.sh lib org.slf4j slf4j-api 1.7.21 lib
 ./install.sh lib org.slf4j slf4j-nop 1.7.21 lib
 
-./install.sh tomcat 8.0.33
+./install.sh tomcat 8.0.35
--- server/src/main/scripts/setenv.sh
+++ server/src/main/scripts/setenv.sh
@@ -8,4 +8,4 @@ export M2_REMOTE_REPO_ORACLE="http://www.datanucleus.org/downloads/maven2"
 
 export scala_vers=("2.12.0-M4")
 export scalaxml_vers=("1.0.5")
-export beangle_server_ver=0.2.5
+export beangle_server_ver=0.2.4
