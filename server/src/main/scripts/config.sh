976afff9d8bc0ad6804c4e9af96e7f2ca5d7e040
段体华 <duantihua@gmail.com>
2019-08-12 21:34:36 +0800

    add aes

--- server/src/main/scripts/config.sh
+++ server/src/main/scripts/config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PRGDIR=`dirname "$0"`
 SAS_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`
-java -cp "$SAS_HOME/bin/lib/*" org.beangle.sas.config.shell.Config $SAS_HOME
+java -cp "$SAS_HOME/bin/lib/*" org.beangle.sas.shell.Config $SAS_HOME
