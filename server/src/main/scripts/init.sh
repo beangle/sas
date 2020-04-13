c7fceb0184ba2e8227da63b6555077a6625551b7
段体华 <duantihua@gmail.com>
2020-04-13 21:41:56 +0800

    bump version 0.6.10

--- server/src/main/scripts/init.sh
+++ server/src/main/scripts/init.sh
@@ -8,7 +8,7 @@ export M2_REPO="$HOME/.m2/repository"
 
 export scala_ver=2.13.1
 export scalaxml_ver=2.0.0-M1
-export beangle_sas_ver=0.6.9
+export beangle_sas_ver=0.6.10
 export beangle_commons_ver=5.1.13
 export beangle_template_ver=0.0.26
 export beangle_data_ver=5.3.4
